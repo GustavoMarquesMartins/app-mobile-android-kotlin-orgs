7acf0726997ac38c2c560b3f4a58a6ad10289c8c
GustavoMarquesMartins <gustavomarquesmartins11@gmail.com>
2024-03-10 21:26:08 -0300

    adicionar valores as views do layout principal

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -11,5 +12,14 @@ class MainActivity : Activity() {
 
         // Define qual arquivo de layout será usado
         setContentView(R.layout.activity_main)
+
+        var nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+
+        var descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja manga, maça"
+
+        var valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
     }
 }
